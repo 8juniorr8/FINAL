42a38ce12ae4d963a7cb4fce0a52187732b127e0
sreitmaier <info@sabinereitmaier.com>
2017-06-22 22:31:39 +0200

    added math library to get const E

--- sketch.js
+++ sketch.js
@@ -1,3 +1,15 @@
+
+var plasticInput;
+var plasticInputYear = plasticInput * 365;
+
+const degrationRate = math.E;
+
+ var x = math.sqrt(-8);
+    console.log(x); // 2i
+    console.log(degrationRate);
+
+
+
 function setup() {
 
 }
